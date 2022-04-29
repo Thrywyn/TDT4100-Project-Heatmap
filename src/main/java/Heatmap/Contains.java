00528cfc6f86c08c0e93a3f83de4773bb3a878a5
Magnus Alexander Strømseng <magnastr@stud.ntnu.no>
2022-04-29 20:13:19 +0200

    oppdatert contains til check whitespace

--- src/main/java/Heatmap/Contains.java
+++ src/main/java/Heatmap/Contains.java
@@ -2,8 +2,19 @@ package Heatmap;
 
 public class Contains {
 
-    public static boolean onlyOneorMoreAlphaNumericSpace(String s) {
-        return s.matches("[a-zA-Z0-9\\s]+");
+    public static boolean onlyOneorMoreAlphaNumericSpaceAndNoWhitespaceAtStartAndEnd(String s) {
+        if (s.matches("[a-zA-Z0-9\\s]+")) {
+            // Check if trailing whitespace
+            if (s.endsWith(" ")) {
+                return false;
+            }
+            // Check if name starts with whitespace
+            if (s.startsWith(" ")) {
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 
 }
